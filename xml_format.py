1a8f00240349eb710ae845e42af2d864db0fa82f
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-05 11:00:56 +0300

    Update xml_format.py

--- xml_format.py
+++ xml_format.py
@@ -1,3 +1,17 @@
+# File: xml_format.py
+# Description: Examples on how to process xml files in Python
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Examples on how to process xml files in Python // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/XML_files_in_Python (date of access: XX.XX.XXXX)
+
+
+
+
 # Working with files in xml format
 
 from xml.etree import ElementTree
